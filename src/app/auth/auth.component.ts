bbdb146468d37a89a4726fd6c4be2bbe77083d1d
Vihang26 <vhpancholi@gmail.com>
2023-02-03 14:23:34 +0530

    Authentication

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,7 +12,9 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent implements OnInit {
 
   isLoginMode = false;
-  constructor() { }
+  isLoading = false;
+  error:string;
+  constructor(private authservice: AuthService, private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -18,8 +23,36 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(form : NgForm){
-    console.log(form.value)
-    form.reset()
+
+    if(!form.valid){
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+    if(this.isLoginMode){
+      authObs =this.authservice.login(email,password);
+    }else{
+      authObs = this.authservice.signup(email,password);
+    }
+
+    authObs.subscribe(
+      resData =>{
+        this.isLoading = false;
+        console.log(resData);
+        this.router.navigate(['/recipes']); 
+      },
+      errorMessage =>{
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+
+    form.reset();
   }
 
 }
